bb0887bf8505d68533d58bede459a4eb2e1ee56c
yolokimo768 <69681974+yolokimo768@users.noreply.github.com>
2020-08-17 18:23:17 +0200

    Update ali Nasr- Drawing.py

--- ali Nasr- Drawing.py
+++ ali Nasr- Drawing.py
@@ -5,12 +5,15 @@ import turtle
 t = turtle.Turtle() #creates Turtle
 
 u= 20
-num = input ('how many times do u want a triangle drawn in a loop ')
+num = input ('how many times do u want a triangle drawn in a loop: ')
+# Adds a color to the drawing 
+color = input('what color would u like the drawings to be: ')
 y = 0 
 
 while y < int(num) :
      y= y+1
      print(y)
+     t.pencolor(color)
      t.right(45)
      t.forward(u*5)
      t.left (135)
